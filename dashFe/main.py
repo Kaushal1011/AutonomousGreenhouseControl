50932d6b4c4afcaa0fb952ef3b5a2896b9bd4156
arpitvaghela <arpitvaghela9210@gmail.com>
2021-04-17 15:25:30 +0530

    fixed dash proto 1

--- dashFe/main.py
+++ dashFe/main.py
@@ -9,51 +9,8 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import requests
-
-# some globals
-actions_assim = []
-rewards_assim = []
-params = [
-    "time",
-    "AssimLight",
-    "BlackScr",
-    "CO2air",
-    "Cum_irr",
-    "EC_drain_PC",
-    "EnScr",
-    "HumDef",
-    "PipeGrow",
-    "PipeLow",
-    "Rhair",
-    "Tair",
-    "Tot_PAR",
-    "Tot_PAR_Lamps",
-    "VentLee",
-    "Ventwind",
-    "co2_dos",
-    "pH_drain_PC",
-    "water_sup",
-    "ProdA",
-    "ProdB",
-    "avg_nr_harvested_trusses",
-    "Truss_development_time",
-    "Nr_fruits_ClassA",
-    "Weight_fruits_ClassA",
-    "Nr_fruits_ClassB",
-    "Weight_fruits_ClassB",
-    "Flavour",
-    "TSS",
-    "Acid",
-    "Juice",
-    "Bite",
-    "Weight",
-    "DMC_fruit",
-    "Stem_elong",
-    "Stem_thick",
-    "Cum_trusses",
-    "stem_dens",
-    "plant_dens",
-]
+import state
+import json
 
 # obs = env_assim.resetinit()
 # obs = list(obs)
@@ -75,60 +32,66 @@ app = dash.Dash(
 
 app.layout = html.Div(
     [
-        # html.Button("Start Simulation", id="start", n_clicks=0),
-        # html.Button("Stop Simulation", id="stop", n_clicks=0),
+        html.Button("Start Simulation", id="start", n_clicks=0),
+        html.Button("Stop Simulation", id="stop", n_clicks=0),
         html.Button("Reset Simulation", id="reset", n_clicks=0),
         dcc.Interval(
-            id="interval-component", interval=100, n_intervals=0  # in milliseconds
+            id="interval-component", interval=1000, n_intervals=0  # in milliseconds
         ),
         dcc.Graph(id="live-update-graph"),
     ]
 )
 
 
-# @app.callback(
-#     dash.dependencies.Output("interval-component", "disable"),
-#     [
-#         dash.dependencies.Input("stop", "n_click"),
-#         dash.dependencies.Input("start", "n_click"),
-#     ],
-# )
-# def start_stop_live(start, stop):
-#     ctx = dash.callback_context
-#     btn_id = ctx.triggered[0]["prop_id"].split(".")[0]
-#     if btn_id == "start":
-#         return True
-#     return False
+@app.callback(
+    dash.dependencies.Output("interval-component", "disabled"),
+    [
+        dash.dependencies.Input("stop", "n_clicks"),
+        dash.dependencies.Input("start", "n_clicks"),
+    ],
+)
+def start_stop_live(start, stop):
+    ctx = dash.callback_context
+    if ctx.triggered[0]["prop_id"].split(".")[0] == "start":
+        return False
+    return True
 
 
-# def reset(n_clicks):
-#     actions_assim = []
-#     rewards_assim = []
-#     # call api to reset
-#     r = requests.get("http://0.0.0.0:8000/light/reset")
-#     if r.status_code != 200:
-#         raise Exception("status response is not 200")
-#     obs = r.json()["data"][0]
-#     # print(obs)
-#     # call api to make predictions
-#     payload = json.dumps({k: v for (k, v) in zip(params, obs)})
-#     # print(payload)
-#     r = requests.post("http://0.0.0.0:8000/light/predict", data=payload)
-#     if r.status_code != 200:
-#         raise Exception("status response is not 200")
-#     actions_assim.append(r.json()["data"][0])
+def reset_graph():
+    state.actions_assim = [0]
+    state.rewards_assim = []
+    # call api to reset
+    r = requests.get("http://0.0.0.0:8000/light/reset")
+    if r.status_code != 200:
+        raise Exception("status response is not 200")
+    obs = r.json()["data"][0]
+    # print(obs)
+    # call api to make predictions
+    payload = json.dumps({k: v for (k, v) in zip(state.params, obs)})
+    # print(payload)
+    r = requests.post("http://0.0.0.0:8000/light/predict", data=payload)
+    if r.status_code != 200:
+        raise Exception("status response is not 200")
+    state.actions_assim.append(r.json()["data"][0])
+    fig = px.line(y=state.actions_assim)
+    return fig
 
 
 @app.callback(
-    Output("live-update-graph", "figure"), Input("interval-component", "n_intervals")
+    Output("live-update-graph", "figure"),
+    [Input("interval-component", "n_intervals"), Input("reset", "n_clicks")],
 )
-def update_graph(n):
-    print("update graph")
-    print(actions_assim)
-    payload = json.dumps({"action": actions_assim[-1]})
+def update_graph(reset, n):
+    ctx = dash.callback_context
+    if ctx.triggered[0]["prop_id"].split(".")[0] == "reset":
+        return reset_graph()
+
+    state.actions_assim.append(n)
+    payload = json.dumps({"action": state.actions_assim[-1]})
+    # print(payload)
     r = requests.post("http://0.0.0.0:8000/light/environment", data=payload)
-    # if r.status_code != 200:
-    #     raise Exception("status response is not 200")
+    if r.status_code != 200:
+        raise Exception("status response is not 200")
     data = r.json()["data"][0]
     obs = data["obs"]
     reward = data["reward"]
@@ -136,16 +99,16 @@ def update_graph(n):
     # print(obs)
     # print(reward)
 
-    rewards_assim.append(reward)
+    state.rewards_assim.append(reward)
     # call api to make predictions
-    payload = json.dumps({k: v for (k, v) in zip(params, obs)})
+    payload = json.dumps({k: v for (k, v) in zip(state.params, obs)})
     r = requests.post("http://0.0.0.0:8000/light/predict", data=payload)
-    # if r.status_code != 200:
-    #     raise Exception("status response is not 200")
-    actions_assim.append(r.json()["data"][0])
-    print(actions_assim)
-    print(rewards_assim)
-    fig = px.line(y=actions_assim)
+    if r.status_code != 200:
+        raise Exception("status response is not 200")
+    state.actions_assim.append(r.json()["data"][0])
+    # print(state.actions_assim)
+    # print(state.rewards_assim)
+    fig = px.line(y=state.actions_assim)
     return fig
 
 
--- dashFe/test2.py
+++
@@ -1,5 +0,0 @@
-import plotly.express as px
-
-l = [1, 2, 3]
-fig = px.line(y=l)
-fig.show()
